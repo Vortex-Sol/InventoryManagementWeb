8eba67be7ccaa08296b198eb3a97dc41f1ed655e
DarkNBolt <mgrombiz@gmail.com>
2025-07-28 09:53:39 +0200

    Fixed: Login
    Logging issue was mainly caused by the login.html file sending the POST request at the wrong API route. Small adjustments have also been made in other java files due to the reason of improper Employee Role load, i.e. Employee's jobs were incorrectly loaded at authentication time - this now works. HTMLs in Templates have been modified to fix a deprocated method of switching to the template the layout.html provides.

--- src/main/java/vortex/imwp/Repositories/EmployeeRepository.java
+++ src/main/java/vortex/imwp/Repositories/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package vortex.imwp.Repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vortex.imwp.Models.Employee;
 
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
     Optional<Employee> findByUsername(String username);
+
+    @Query("SELECT e FROM Employee e LEFT JOIN FETCH e.jobs WHERE e.username = :username")
+    Optional<Employee> findByUsernameWithJobs(@Param("username") String username);
 }
