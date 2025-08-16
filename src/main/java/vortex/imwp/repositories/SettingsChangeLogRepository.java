7fbf4d31ad5ae85fe3cedaa2204b3af3681c7832
Mkonrad-afk <s29778@pjwstk.edu.pl>
2025-08-16 21:33:51 +0200

    settings log - id fixed

--- src/main/java/vortex/imwp/repositories/SettingsChangeLogRepository.java
+++ src/main/java/vortex/imwp/repositories/SettingsChangeLogRepository.java
@@ -1,8 +1,22 @@
 package vortex.imwp.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import vortex.imwp.models.SettingsChangeAudit;
-import vortex.imwp.models.SettingsChangeAuditID;
+import vortex.imwp.models.SettingsChangeLog;
 
-public interface SettingsChangeLogRepository extends JpaRepository<SettingsChangeAudit, SettingsChangeAuditID> {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface SettingsChangeLogRepository extends JpaRepository<SettingsChangeLog, Long> {
+
+	List<SettingsChangeLog> findAllByOrderByChangedAtDesc();
+	Page<SettingsChangeLog> findAllByOrderByChangedAtDesc(Pageable pageable);
+
+	Page<SettingsChangeLog> findByAdminIdAndChangedAtBetweenOrderByChangedAtDesc(
+			Long adminId, LocalDateTime from, LocalDateTime to, Pageable pageable);
+
+	Page<SettingsChangeLog> findByWarehouseIdOrderByChangedAtDesc(Long warehouseId, Pageable pageable);
+
+	Page<SettingsChangeLog> findBySettingIdOrderByChangedAtDesc(Long settingId, Pageable pageable);
 }
