b1ccf5dda808c3ce650aa4a22f544237ef78cb3a
DarkNBolt <mgrombiz@gmail.com>
2025-08-20 14:57:48 +0200

    [5B] contact.page-patch

--- src/main/java/vortex/imwp/controllers/HomeController.java
+++ src/main/java/vortex/imwp/controllers/HomeController.java
@@ -2,17 +2,26 @@ package vortex.imwp.controllers;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import vortex.imwp.models.*;
+import vortex.imwp.mappers.WarehouseDTOMapper;
+import vortex.imwp.models.Employee;
+import vortex.imwp.models.Warehouse;
 import vortex.imwp.services.EmployeeService;
+import vortex.imwp.services.WarehouseService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/home")
 public class HomeController {
     private final EmployeeService employeeService;
-    public HomeController(EmployeeService employeeService) {
+    private final WarehouseService warehouseService;
+
+    public HomeController(EmployeeService employeeService,  WarehouseService warehouseService) {
         this.employeeService = employeeService;
+        this.warehouseService = warehouseService;
     }
 
     @GetMapping
@@ -26,5 +35,17 @@ public class HomeController {
             else if(employee.getJobs().stream().anyMatch(job -> "SUPERADMIN".equals(job.getName()))) return "redirect:/api/super";
         }
         return "/error";
+    }
+
+    @GetMapping("/contact")
+    public String contact(Authentication auth, Model model) {
+        Optional<Warehouse> warehouse = warehouseService.getWarehouseById(employeeService.getEmployeeByAuthentication(auth).getWarehouseID());
+        if(warehouse.isPresent()) {
+            model.addAttribute("warehouse", WarehouseDTOMapper.map(warehouse.get()));
+        }
+        else{
+            model.addAttribute("error", "Warehouse not found");
+        }
+        return "contact-information";
     }
 }
